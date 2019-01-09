bb018f8d359c5bad49ef1e9f2205634962503016
Josip Igrec <me@allypost.net>
2019-01-09 23:53:57 +0100

    Employee-o-matic employees can now have different roles

--- employee-o-matic.rb
+++ employee-o-matic.rb
@@ -14,6 +14,67 @@ class Employee
   def surname
     @full_name.split(' ', 2).last
   end
+
+  def to_s
+    "#{full_name}, #{id}"
+  end
+end
+
+class Programmer < Employee
+  attr_reader :languages
+
+  def initialize(full_name, id, languages)
+    super(full_name, id)
+    @languages = languages
+  end
+
+  def languages=(value)
+    @languages = parse_languages(value)
+  end
+
+  def to_s
+    "#{super} #{@languages.inspect}"
+  end
+
+  private
+
+  def parse_languages(value)
+    return value if value.is_a?(Array)
+    return value.chomp.split(',').map(&:strip) if value.is_a?(String)
+
+    @languages || []
+  end
+end
+
+class OfficeManager < Employee
+  attr_accessor :office
+
+  def initialize(full_name, id, office)
+    super(full_name, id)
+    @office = office
+  end
+
+  def to_s
+    "#{super} (#{@office})"
+  end
+end
+
+def create_employee(full_name, id, type)
+  case type
+  when 'e' then
+    Employee.new(full_name, id)
+  when 'p' then
+    print 'Programming languages (comma separated): '
+    languages = gets.chomp.split(',').map(&:strip)
+    Programmer.new(full_name, id, languages)
+  when 'o' then
+    print 'Office: '
+    office = gets.chomp
+    OfficeManager.new(full_name, id, office)
+  else
+    puts 'Employee type not recognized.'
+    nil
+  end
 end
 
 def add_employee(employees)
@@ -22,15 +83,18 @@ def add_employee(employees)
   full_name = gets.chomp
   print 'ID: '
   id = gets.chomp
+  print 'Is the person an [e]mployee, [p]rogrammer or an [o]ffice manager? '
+  type = gets.chomp.downcase
 
-  employee = Employee.new(full_name, id)
-
-  employees << employee
+  employee = create_employee(full_name, id, type)
+  employees << employee unless employee.nil?
 end
 
 def edit_property(employee, property_key, property_name)
-  old_value = employee.send(property_key)
-  print "#{property_name} [#{old_value}]: "
+  print_value = employee.send(property_key)
+  print_value = print_value.join(', ') if print_value.is_a?(Array)
+
+  print "#{property_name} [#{print_value}]: "
   new_value = gets.chomp
 
   employee.send("#{property_key}=", new_value) unless new_value.strip.empty?
@@ -49,6 +113,8 @@ def edit_employee(employees)
   puts 'Employee found. Leave field blank to keep old value.'
   edit_property(employee, :full_name, 'Full name')
   edit_property(employee, :id, 'ID')
+  edit_property(employee, :languages, 'Languages') if employee.is_a?(Programmer)
+  edit_property(employee, :office, 'Office') if employee.is_a?(OfficeManager)
 end
 
 def get_sort_key
@@ -73,9 +139,7 @@ def view_employees(employees)
     key = get_sort_key
   end
 
-  sorted_employees(employees, key).each do |employee|
-    puts "#{employee.full_name}, #{employee.id}"
-  end
+  sorted_employees(employees, key).each(&method(:puts))
 end
 
 def sorted_employees(employees, key)

7812f39ad51d1a7fce29f95b94fbb698040357ac
Josip Igrec <me@allypost.net>
2019-01-15 15:51:38 +0100

    Chapter intro done

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'landing#index' # Add this line
+
+  get 'polynomials/new', to: 'polynomials#new'
+  get 'polynomials/:id', to: 'polynomials#show'
+  post 'polynomials', to: 'polynomials#create'
 end
